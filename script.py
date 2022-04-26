492f902ef3891cccc2a9ca0a7e261176c6f3897a
amitai.getzler <amitai.getzler@explorium.ai>
2022-04-26 13:28:24 +0300

    update

--- script.py
+++ script.py
@@ -1,43 +1,71 @@
 import os
 import yaml
 import subprocess
+from git import Repo
 
 dct = yaml.load(os.environ["CHARTS"],yaml.Loader)
 for key in dct:
     helm_name = key
     helm_options = dct[key]
-    subprocess.run(
-        [
-            "helm", 
-            "repo",
-            "add",
-            helm_options["repo_name"],
-            helm_options["repo"]
-        ]
-    )
-    subprocess.run(
-        [
-            "helm", 
-            "upgrade",
-            "-i",
-            helm_name,
-            "--wait",
-            "--create-namespace",
-        ] 
-        + 
-        ([
-            "--namespace={}".format(helm_options["namespace"])
-        ] if ("namespace" in helm_options) else [])
-        + 
-        ([
-            "--version={}".format(helm_options["version"])
-        ] if ("version" in helm_options) else [])        
-        + 
-        ([
-            "--set={key}={value}".format(key = x["key"], value = x["value"]) for x in helm_options["values"]
-        ] if ("values" in helm_options) else [])
-        +
-        [
-            helm_options["repo_name"]+"/"+helm_options["chart"]
-        ]
-    )
+    if helm_options["type"] == "helm":
+        subprocess.run(
+            [
+                "helm", 
+                "repo",
+                "add",
+                helm_options["repo_name"],
+                helm_options["repo"]
+            ]
+        )
+        subprocess.run(
+            [
+                "helm", 
+                "upgrade",
+                "-i",
+                helm_name,
+                "--wait",
+                "--create-namespace",
+            ] 
+            + 
+            ([
+                "--namespace={}".format(helm_options["namespace"])
+            ] if ("namespace" in helm_options) else [])
+            + 
+            ([
+                "--version={}".format(helm_options["version"])
+            ] if ("version" in helm_options) else [])        
+            + 
+            ([
+                "--set={key}={value}".format(key = x["key"], value = x["value"]) for x in helm_options["values"]
+            ] if ("values" in helm_options) else [])
+            +
+            [
+                helm_options["repo_name"]+"/"+helm_options["chart"]
+            ]
+        )
+    elif helm_options["type"] == "git":
+        Repo.clone_from(helm_options["repo"], helm_name)
+        os.chdir(helm_name)
+        subprocess.run(
+            [
+                "helm", 
+                "upgrade",
+                "-i",
+                helm_name,
+                "--wait",
+                "--create-namespace",
+            ] 
+            + 
+            ([
+                "--namespace={}".format(helm_options["namespace"])
+            ] if ("namespace" in helm_options) else [])
+            + 
+            ([
+                "--set={key}={value}".format(key = x["key"], value = x["value"]) for x in helm_options["values"]
+            ] if ("values" in helm_options) else [])
+            +
+            [
+                "."
+            ]
+        )
+        os.chdir(os.path.dirname(os.getcwd()))
