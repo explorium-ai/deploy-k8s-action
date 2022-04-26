91baa78f7068978f50e4e3ffe2168b4a655b8363
amitai.getzler <amitai.getzler@explorium.ai>
2022-04-26 12:45:08 +0300

    update

--- script.py
+++ script.py
@@ -14,7 +14,22 @@ for key in dct:
             helm_options["repo_name"],
             helm_options["repo"]
         ]
-    )    
+    )
+    print([
+            "helm", 
+            "upgrade",
+            "-i",
+            helm_name,
+            "--wait",
+            "--namespace={}".format(helm_options["namespace"]) if ("namespace" in helm_options) else "",
+            "--create-namespace",
+            "--version={}".format(helm_options["version"]) if ("version" in helm_options) else "",
+            helm_options["repo_name"]+"/"+helm_options["chart"]
+        ] 
+        + 
+        ([
+            "--set={key}={value}".format(key = x["key"], value = x["value"]) for x in helm_options["values"]
+        ] if ("values" in helm_options) else []))
     subprocess.run(
         [
             "helm", 
